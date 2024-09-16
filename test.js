9791d78933592b6dbab808fd345f02e452580f17
xiuxiu <xiuxiu@xiuxiudeMacBook-Pro.local>
2024-09-16 11:19:12 -0700

    doneforweek03

---
+++ test.js
@@ -0,0 +1,18 @@
+//string the word "hobby" to "fav thing"
+//const should be "hobby"
+//let should be "peaceful"
+
+const hobby = "fav thing";
+let peaceful = "nice";
+
+console.log("Hiking is my " + hobby + " because it brings a " + peaceful + " everyday life. As a" + hobby + " , it offers me the chance to connect with nature and find peace in the quiet surroundings. Whether I/'m walking along a " + peaceful + " forest trail or hiking up a mountain, this " + hobby + " always fills me with a sense of calm and tranquility. There/'s something so " + peaceful + " about the rhythm of hiking, the fresh air, and the beauty of the landscapes. It/'s a " + hobby + " that truly brings me peace every time I go out.");
+
+peaceful = "good";
+
+console.log("Every week, I look forward to my free time, where I can enjoy my favorite " + hobby + " in a " + peaceful + " way. One of my most " + peaceful + " " + hobby + " is going for hikes or long walks, but my favorite " + hobby + " is playing video games. Even though it’s more active, video games can feel " + peaceful + " as I immerse myself in different worlds, solve puzzles, and complete missions at my own pace. This " + hobby + " offers a perfect balance of excitement and relaxation, making my free time both enjoyable and " + peaceful + "");
+
+
+// Everything seems perfect!
+
+
+
